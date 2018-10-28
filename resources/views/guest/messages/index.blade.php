b5c24efcb8105ecf20e408e0b5313f40027433dd
Ivann Nuggets <35358638+nuGuith@users.noreply.github.com>
2018-10-29 00:07:23 +0800

    updated messages.index.blade.php

--- resources/views/guest/messages/index.blade.php
+++ resources/views/guest/messages/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.master')
+@extends('admin.default')
 
 @section('content')
 
@@ -12,13 +12,11 @@
                       <!-- Header -->
                       <div class="peers fxw-nw jc-sb ai-c pY-20 pX-30 bgc-white">
                         <div class="peers ai-c">
-                          <div class="peer d-n@md+">
-                            <a href="" title="" id='chat-sidebar-toggle' class="td-n c-grey-900 cH-blue-500 mR-30">
-                              <i class="ti-menu"></i>
-                            </a>
+                          <div class="peer mR-20">
+                            <img src="https://randomuser.me/api/portraits/women/12.jpg" alt="" class="w-3r h-3r bdrs-50p">
                           </div>
                           <div class="peer mR-20">
-                            <img src="https://randomuser.me/api/portraits/men/12.jpg" alt="" class="w-3r h-3r bdrs-50p">
+                            Messages / <small>from admin </small>
                           </div>
                         </div>
                         <div class="peers">
@@ -28,7 +26,7 @@
                         </div>
                       </div>
                     </div>
-                    <div class="layer w-100 fxg-1 bgc-grey-200 scrollable pos-r">
+                    <div id="chatBox" class="layer w-100 fxg-1 bgc-grey-200 scrollable pos-r">
                       <!-- Chat Box -->
                       <!-- {{ $prev = auth()->user()->id }} -->
                       <!-- {{ $user_id = auth()->user()->id }} -->
@@ -37,7 +35,7 @@
                         <!-- Chat Conversation -->
                         <div class="peers fxw-nw {{ $m->user_id == $user_id ? 'ai-fe' : 'ai-fs' }}">
                           <div class="peer {{ $m->user_id == $user_id ? 'ord-1 mL-20' : 'ord-0 mR-20' }}">
-                            <img class="w-2r bdrs-50p" src="https://randomuser.me/api/portraits/men/{{ $m->user_id == $user_id ? '12' : '11' }}.jpg" alt="avatar">
+                            <img class="w-2r bdrs-50p" src="https://randomuser.me/api/portraits/women/{{ $m->user_id == $user_id ? '12' : '11' }}.jpg" alt="avatar">
                           </div>
                           <div class="peer peer-greed {{ $m->user_id == $user_id ? 'ord-0' : 'ord-1' }}">
                             <div class="layers {{ $m->user_id == $user_id ? 'ai-fe gapY-10' : 'ai-fs gapY-5' }}">
@@ -47,7 +45,7 @@
                                   <div class="peer {{ $m->user_id == $user_id ? 'mL-10 ord-1' : 'mR-10 ord-0' }} ">
                                     <small>{{ date('H:iA', strtotime($m->time)) }}</small>
                                   </div>
-                                  <div class="peer-greed ord-0">
+                                  <div class="peer-greed {{ $m->user_id == $user_id ? 'ord-0' : 'ord-1' }}">
                                     <span>{{ $m->content }}</span>
                                   </div>
                                 </div>
@@ -57,29 +55,53 @@
                           </div>
                         </div>
                       </div>
-                      @if($prev != $m->user_id)
-                        <!-- {{$prev}} -->
-                        <!-- {{$prev = $m->user_id}} -->
-                      @endif
                       @endforeach
                     </div>
                     <div class="layer w-100">
                       <!-- Chat Send -->
                       <div class="p-20 bdT bgc-white">
                         <div class="pos-r">
-                          <input type="text" class="form-control bdrs-10em m-0" placeholder="Say something...">
+                        {{Form::open(array('id' => 'messageForm'))}}
+                          <input type="hidden" name="header_id" value="{{$header_id}}">
+                          <input type="text" name="message" id="message" class="form-control bdrs-10em m-0" placeholder="Say something...">
                           <button id="btnSend" type="button" class="btn btn-primary bdrs-50p w-2r p-0 h-2r pos-a r-1 t-1">
                             <i class="fa fa-paper-plane-o"></i>
                           </button>
+                        {{Form::close()}}
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
             </div>
+  <script type="text/javascript" src="{{URL('js/jquery.min.js')}}"></script>
   <script>
-  $('#btnSend').on('click', function(){
-    alert("u clicked");
+  $(document).ready(function(){
+    $("#chatBox").animate({ scrollTop: $('#chatBox').prop("scrollHeight")}, 1000);
+    $('#btnSend').prop('disabled', true);
+    
+    $('#message').on('keyup', function(){
+      var msg = $(this).val();
+      if(msg == null || msg == "")
+        $('#btnSend').prop('disabled', true);
+      else
+        $('#btnSend').prop('disabled', false);
+    });
+
+    $('#btnSend').on('click', function(){
+      if($('#message').val() != null || $('#message').val() != ""){
+        var message = $('#messageForm').serialize();
+        $.ajax({
+          headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
+          type: 'POST',
+          url: '/admin/message',
+          data: message,
+          success:function(data){
+            window.location.href = '/admin/message';
+          }
+        });
+      }
+    });
   });
   </script>
 @endsection
